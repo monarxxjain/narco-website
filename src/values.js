2b87212246e6de8cd2213f6e082b629f57d4e71e
Harun Biswas Rubel <harunbiswasrubel@gmail.com>
2023-11-02 04:29:31 +0600

    update

--- src/values.js
+++ src/values.js
@@ -1,6 +1,6 @@
 const values = {};
 
-// values.url = "https://marco-backend-update.vercel.app";
-values.url = "http://localhost:5000";
+values.url = "https://marco-backend-update.vercel.app";
+// values.url = "http://localhost:5000";
 
 export default values;
