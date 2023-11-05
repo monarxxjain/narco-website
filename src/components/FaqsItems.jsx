07204bb95f49f2f743ad8b4eae94559614c80308
monarxxjain <jainmonark1000@gmail.com>
2023-11-05 17:42:08 +0530

    IF/II F 0.23 + 0.24 :: Fixed

--- src/components/FaqsItems.jsx
+++ src/components/FaqsItems.jsx
@@ -3,6 +3,12 @@ import { CheckIcon, Minus, Plus } from "./Icon";
 
 export const FaqsItems = ({ data, id }) => {
   const [open, setOpen] = useState(0);
+  const sex = [
+    { agelimit: 0, discount: 0 },
+    { agelimit: 2, discount: -50 },
+    { agelimit: 4, discount: -20 }
+  ];
+
   return (
     <div className="accordion" id={id}>
       {Array.isArray(data) &&
@@ -67,27 +73,59 @@ export const FaqsItems = ({ data, id }) => {
                         ))}
                       </ul>
                     )}
-                    {data && data.length && (
+                    {/* {sex && sex.length && (
                       <ul>
-                        {data?.map((item, i) => (
-                          <li key={i}>
-                            <p>
-                              Bambini da{" "}
-                              {(data.length === 1 && "0") ||
-                              data[i - 1]?.agelimit + 1 < data[i]?.agelimit
-                                ? data[i - 1]?.agelimit + 1
-                                : (data.length > 1 &&
-                                    data[i - 2]?.agelimit + 1) ||
-                                  0}{" "}
-                              a {item?.agelimit} anni (esclusi):{" "}
-                              {Math.abs(item?.discount)}
-                              {(item?.discount &&
-                                item?.discount > -1 &&
-                                "%  di sconto") ||
-                                "€ (al giorno)"}
-                            </p>
-                          </li>
-                        ))}
+                        {sex.map((item, i) => {
+                          const ageRangeStart =
+                            (sex.length === 1 && 0) ||
+                            (sex[i - 1]?.agelimit + 1 < item?.agelimit
+                              ? sex[i - 1]?.agelimit + 1
+                              : (sex.length > 1 && sex[i - 2]?.agelimit + 1) || 0);
+
+                          const discountValue = Math.abs(item?.discount);
+
+                          let discountInfo = "";
+                          if (item?.discount && discountValue > 0) {
+                            discountInfo = `${discountValue}€ al giorno`;
+                          } else if (item?.discount && discountValue === 0) {
+                            discountInfo = "Gratis";
+                          }
+
+                          return (
+                            <li key={i}>
+                              {`Bambini da ${ageRangeStart} a ${item?.agelimit - 0.01}: ${discountInfo}`}
+                            </li>
+                          );
+                        })}
+                      </ul>
+                    )} */}
+
+                    {data && data.length > 0 && (
+                      <ul>
+                        {data.map((item, i) => {
+                          let ageRangeStart = 0;
+                          if (i > 0) {
+                            ageRangeStart = data[i - 1]?.agelimit + 1;
+                          }
+
+                          const ageRangeEnd = item?.agelimit - 0.01;
+                          const discountValue = Math.abs(item?.discount);
+
+                          let discountInfo = "";
+                          if (item?.discount) {
+                            if (discountValue > 0) {
+                              discountInfo = `${discountValue}% di sconto`;
+                            } else {
+                              discountInfo = "Gratis";
+                            }
+                          }
+
+                          return (
+                            <li key={i}>
+                              {`Bambini da ${ageRangeStart} a ${ageRangeEnd}: ${discountInfo}`}
+                            </li>
+                          );
+                        })}
                       </ul>
                     )}
                   </div>
